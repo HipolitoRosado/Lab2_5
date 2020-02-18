9c8420808c2803de67d5267c32258b19a1c33118
HipolitoRosado <61118445+HipolitoRosado@users.noreply.github.com>
2020-02-18 13:29:15 -0600

    Add files via upload

---
+++ HW2/HW2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hipolito");
+            Console.WriteLine("Rosado");
+
+        }
+    }
+}
