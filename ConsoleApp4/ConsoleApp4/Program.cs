d2a796d11b4a62b207ff678343905cf9ab511481
HipolitoRosado <61118445+HipolitoRosado@users.noreply.github.com>
2020-02-18 14:34:09 -0600

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            int number1, number2, number3;
+            Console.WriteLine("Enter first number");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third number");
+            number3 = Convert.ToInt32(Console.ReadLine());
+            int result = number1 * number2 * number3;
+            Console.WriteLine("The answer for the multiplication of your 3 numbers is " +result);
+          
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int Age;
+;            Console.WriteLine("What is your Age?");
+            Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your just the right age :-)");
+
+        }
+    }
+}
